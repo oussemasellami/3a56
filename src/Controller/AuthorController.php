5a21f2b8f57182c16acff492538732c37d638f8d
oussema.sellami <oussema.sellami@esprit.tn>
2023-10-02 10:57:57 +0100

    first entity author + show

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+
+    public $authors = array(
+        array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+        array('id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200),
+        array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
+    );
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -16,17 +23,39 @@ class AuthorController extends AbstractController
         ]);
     }
 
-    #[Route('/showauthor/{name}', name: 'app_showauthor')]
-    public function showauthor($name): Response
+    #[Route('/showDBauthor', name: 'showDBauthor')]
+    public function showDBauthor(AuthorRepository $authorRepository): Response
     {
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200),
-            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
-        );
+        $authors = $authorRepository->findAll();
+
+        return $this->render('author/showDBauthor.html.twig', [
+            'authors' => $authors
+        ]);
+    }
+
+    #[Route('/showauthor', name: 'app_showauthor')]
+    public function showauthor(): Response
+    {
+
         return $this->render('author/show.html.twig', [
-            'namehtml' => $name,
-            'authorshtml' => $authors
+            'authorshtml' => $this->authors,
+        ]);
+    }
+
+    #[Route('/authorDetails/{id}', name: 'authorDetails')]
+    public function authorDetails($id): Response
+    {
+        //var_dump($id) . die();
+
+        $author = null;
+        foreach ($this->authors as $authorData) {
+            if ($authorData['id'] == $id) {
+                $author = $authorData;
+            }
+        }
+
+        return $this->render('author/details.html.twig', [
+            'author' => $author
         ]);
     }
 }
